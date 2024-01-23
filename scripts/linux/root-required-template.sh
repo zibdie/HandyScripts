2d9cc1c73ec75450824cc5f8537df83d6add7537
zibdie <59550818+zibdie@users.noreply.github.com>
2024-01-23 09:56:44 -0500

    Updated script template and name

--- scripts/linux/root-required-template.sh
+++ scripts/linux/root-required-template.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# RUN YOUR 'ROOT REQUIRED' COMMANDS HERE! 
-# You dont need to add sudo, can work with or without!
+# Template to run root-required commands easily
+# Adding 'sudo' not required and works without it either way.
+
+# Remember to 'chmod +x root-required-template.sh' 
+
+# Write your root-required commands here. COMMANDS variable is multi-line
 COMMANDS="
 sudo apt-get update
 sudo apt-get dist-upgrade -y
 sudo apt-get autoremove -y
 "
 
-# Function to check if user is in the sudo group
 is_in_sudo_group() {
     if id -nG "$USER" | grep -qw "sudo"; then
         return 0
@@ -17,7 +20,6 @@ is_in_sudo_group() {
     fi
 }
 
-# Function to check if sudo can be executed without a password
 can_sudo_without_password() {
     if sudo -n true 2>/dev/null; then
         return 0
@@ -26,25 +28,20 @@ can_sudo_without_password() {
     fi
 }
 
-# Function to execute the commands with sudo, prompt for password if needed
 run_commands() {
     if [[ $(id -u) -eq 0 ]]; then
         echo "Running as root, no need for sudo."
-        # Execute commands without sudo as root
         eval "${COMMANDS//sudo /}"
     elif is_in_sudo_group && can_sudo_without_password; then
-        echo "User has passwordless sudo access."
-        # Execute commands as they are
+        echo "User '$USER' has passwordless sudo access. Continuing..."
         eval "$COMMANDS"
     elif is_in_sudo_group; then
-        echo "User is in sudo group, prompting for password..."
-        # Execute commands as they are
+        echo "User '$USER'  is in sudo group, prompting for password..."
         eval "$COMMANDS"
     else
-        echo "Error: User is not in sudo group. Cannot perform operations requiring elevated privileges."
+        echo "Error: User '$USER' is not in sudo group. Cannot perform operations requiring elevated privileges."
         exit 1
     fi
 }
 
-# Run the commands
 run_commands
